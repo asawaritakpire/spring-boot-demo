6f1371b48aa7daf59588121b4870ae15800ed1ab
Asawari Takpire <asawaritakpire16@gmail.com>
2024-03-06 23:05:30 +0530

    added devtool and actuator dependency

--- src/main/java/com/springboot/demo/demoapp/rest/DemoRestController.java
+++ src/main/java/com/springboot/demo/demoapp/rest/DemoRestController.java
@@ -3,6 +3,8 @@ package com.springboot.demo.demoapp.rest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.PublicKey;
+
 @RestController
 public class DemoRestController {
 
@@ -10,5 +12,11 @@ public class DemoRestController {
     public String getMethoddemo()
     {
         return "Hello Asawari!!";
+    }
+
+    @GetMapping("/name")
+    public String getName()
+    {
+        return "Asawari";
     }
 }
