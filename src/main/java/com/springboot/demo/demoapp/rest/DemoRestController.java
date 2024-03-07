91541477185003b4d8cb8951671321a2307073c4
Asawari Takpire <asawaritakpire16@gmail.com>
2024-03-07 20:04:46 +0530

    injecting custom application properties in controller

--- src/main/java/com/springboot/demo/demoapp/rest/DemoRestController.java
+++ src/main/java/com/springboot/demo/demoapp/rest/DemoRestController.java
@@ -1,5 +1,6 @@
 package com.springboot.demo.demoapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,15 +9,20 @@ import java.security.PublicKey;
 @RestController
 public class DemoRestController {
 
+
+
+    @Value("${author.name}")
+    private String authorName;
+
     @GetMapping("/")
     public String getMethoddemo()
     {
-        return "Hello Asawari!!";
+        return "Hello " + authorName;
     }
 
     @GetMapping("/name")
     public String getName()
     {
-        return "Asawari";
+        return authorName;
     }
 }
